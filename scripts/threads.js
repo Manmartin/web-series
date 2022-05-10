251fda15abb59ec497162c4eecf057a06675a5fb
Manmartin <manuelmartinlopez02@gmail.com>
2022-05-10 21:43:18 +0200

    Threads improved

--- scripts/threads.js
+++ scripts/threads.js
@@ -1,4 +1,9 @@
-import { makeRequest } from "./requests.js";
+import {
+    makeRequest
+} from "./requests.js";
+
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'eleven'];
+
 
 const form = document.getElementById("post-form")
 form.addEventListener('submit', (e) => {
@@ -9,7 +14,9 @@ form.addEventListener('submit', (e) => {
 async function sendData(form) {
     try {
         const formData = new FormData(form);
-        const data = {"data": {}}
+        const data = {
+            "data": {}
+        }
         var queryString = new URLSearchParams(formData)
 
         data.data.name = queryString.get('name');
@@ -17,7 +24,10 @@ async function sendData(form) {
         data.data.serie = serieID;
         data.data.users_permissions_user = localStorage.getItem('userID');
 
-        await makeRequest('http://localhost:1337/api/threads', 'POST', JSON.stringify(data),  {"Content-Type" : "application/json" ,"Authorization": `Bearer ${userToken}`}, putThread);
+        await makeRequest('http://localhost:1337/api/threads', 'POST', JSON.stringify(data), {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${userToken}`
+        }, putThread);
     } catch (error) {
         console.log(error)
     }
@@ -26,40 +36,64 @@ async function sendData(form) {
 
 
 function putThread(data) {
+    
+
+    const lista = document.getElementById('accordionExample');
+    const div = document.createElement('div')
     const thread = data.data.attributes;
 
-    const link = document.createElement('a');
-    link.href = `comments.html?id=${data.data.id}`;
-    const name = document.createElement('h2');
-    name.innerText = thread.name;
-    const content = document.createElement('h2');
-    content.innerText = thread.content;
-
-    link.appendChild(name);
-    link.appendChild(content);
-    document.body.appendChild(link);
+    div.classList.add("accordion-item");
+    div.innerHTML = `
+    <h2 class="accordion-header" id="heading${count}">
+      <button style="text-transform: capitalize;" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${count}" aria-expanded="true" aria-controls="collapse${count}">
+        ${thread.name}
+      </button>
+    </h2>
+    <div id="collapse${count}" class="accordion-collapse collapse" aria-labelledby="heading${count}" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <a href="comments.html?id=${data.data.id}">${thread.content}</a>
+      </div>
+    </div>
+  `
+    count++;
+    lista.appendChild(div);
 }
 
+
+/* Preguntar por paginacion */
+
+let count = 0;
+
 function printData(data) {
+    const lista = document.getElementById('accordionExample');
+
     let threads = data.data.attributes.threads.data;
     for (const thread of threads) {
-        const link = document.createElement('a');
-        link.href = `comments.html?id=${thread.id}`;
-        const name = document.createElement('h2');
-        name.innerText = thread.attributes.name;
-        const content = document.createElement('h2');
-        content.innerText = thread.attributes.content;
-    
-        link.appendChild(name);
-        link.appendChild(content);
-        document.body.appendChild(link);
+        const div = document.createElement('div');
+        div.classList.add("accordion-item");
+        div.innerHTML = `
+        <h2 class="accordion-header" id="heading${count}">
+          <button style="text-transform: capitalize;" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${count}" aria-expanded="true" aria-controls="collapse${count}">
+            ${thread.attributes.name}
+          </button>
+        </h2>
+        <div id="collapse${count}" class="accordion-collapse collapse${ count === 0 ? 'show' : " "}" aria-labelledby="heading${count}" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+          <a href="comments.html?id=${thread.id}">${thread.attributes.content}</a>
+          </div>
+        </div>
+      `
+        lista.appendChild(div);
+        count++;
     }
 }
 
 
 const serieID = (new URLSearchParams(window.location.search)).get('id');
 if (serieID) {
-    await makeRequest(`http://localhost:1337/api/series/${serieID}?populate=threads`, 'GET', null, {Authorization: `Bearer ${userToken}`}, printData);
+    await makeRequest(`http://localhost:1337/api/series/${serieID}?populate=threads`, 'GET', null, {
+        Authorization: `Bearer ${userToken}`
+    }, printData);
 } else {
     window.location.href = 'index.html';
 }
